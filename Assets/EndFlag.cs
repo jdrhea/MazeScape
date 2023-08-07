857bc0c29eb00dd6b0487b2931055efced805057
jdrhea <93232701+jdrhea@users.noreply.github.com>
2023-08-06 20:47:45 -0500

    hp update

---
+++ Assets/EndFlag.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndFlag : MonoBehaviour
+{
+    public GameObject pointc;
+    public GameObject pointd;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointd.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointd.transform)
+        { 
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointd.transform)
+        {
+            currentPoint = pointc.transform;
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointc.transform)
+        {
+            currentPoint = pointd.transform;
+        }
+        
+        
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if (collision.CompareTag("player"))
+       {
+            Debug.Log("YOU WIN!!");
+       } 
+    }
+}
+
--- Assets/ObjectCollector.cs
+++ Assets/ObjectCollector.cs
@@ -4,22 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CoinCollector : MonoBehaviour
+
+public class ObjectCollector : MonoBehaviour
 {
     public Text score;
     private int scoreValue = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "coin")
+        if (collision.gameObject.tag == "sword")
         {
             collision.gameObject.SetActive(false);
             scoreValue += 1;
             SetScore();
         }
+        
+    }
+    void SetScore(){
+        score.text = "Inventory: " + "Sword";
+
+        
     }
 
-    void SetScore(){
-        score.text = "$" + scoreValue;
-    }
 }
